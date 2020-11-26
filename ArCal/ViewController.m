e1e6301172cb06c18c1ef96aa0df894e309a0676
Shehzad Daredia <shehzad.daredia@gmail.com>
2020-11-25 20:58:52 -0600

    Parse response json for event data and load it into the table view

--- ArCal/ViewController.m
+++ ArCal/ViewController.m
@@ -21,7 +21,6 @@ NSString *cellId = @"cellId";
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self setupEvents];
     [self fetchEvents];
 
 //    self.view.backgroundColor = [UIColor yellowColor];
@@ -39,24 +38,58 @@ NSString *cellId = @"cellId";
 
     [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
 
-        NSString *responseJson = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        NSLog(@"response json: %@", responseJson);
+        NSError *err;
+        NSDictionary *responseDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&err];
+        if (err) {
+            NSLog(@"Failed to serialize into JSON: %@", err);
+            return;
+        }
 
+        NSArray *responseEvents = responseDict[@"items"];
+        self.events = NSMutableArray.new;
+
+        for (NSDictionary *eventDict in responseEvents) {
+            NSString *startDate = eventDict[@"start"][@"date"];
+            NSString *endDate = eventDict[@"end"][@"date"];
+            NSString *startTime = eventDict[@"start"][@"dateTime"];
+            NSString *endTime = eventDict[@"end"][@"dateTime"];
+
+            Event *event = Event.new;
+            event.title = eventDict[@"summary"];
+            event.startDate = [self convertDatetimeStringToNSDate:startDate];
+            event.endDate = [self convertDatetimeStringToNSDate:endDate];
+            event.startTime = [self convertDatetimeStringToNSDate:startTime];
+            event.endTime = [self convertDatetimeStringToNSDate:endTime];
+            [self.events addObject:event];
+        }
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
     }] resume];
-}
-
-- (void)setupEvents {
-    self.events = NSMutableArray.new;
-
-    Event *event = Event.new;
-    event.title = @"Study iOS";
-    event.startTime = NSDate.new;
-    event.endTime = NSDate.new;
-    [self.events addObject:event];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.events.count;
+}
+
+/**
+ * Converts a date or datetime string to an NSDate object.
+ */
+- (NSDate *)convertDatetimeStringToNSDate:(NSString *)datetime {
+    NSDateFormatter *datetimeFormatter = NSDateFormatter.new;
+    [datetimeFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
+    [datetimeFormatter setTimeZone:[NSTimeZone systemTimeZone]];
+    NSDate *date = [datetimeFormatter dateFromString:datetime];
+    if (date != nil) {
+        return date;
+    }
+
+    NSDateFormatter *dateFormatter = NSDateFormatter.new;
+    [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+    [dateFormatter setTimeZone:[NSTimeZone systemTimeZone]];
+    // TODO(shez): throw or otherwise handle if this is nil
+    return [dateFormatter dateFromString:datetime];
 }
 
 - (NSString *)getEventCellText:(Event *)event {
