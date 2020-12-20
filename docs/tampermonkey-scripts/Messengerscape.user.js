db135670ef64999deed16eee0a5db026aa4bf08c
Shehzad Daredia <shehzad.daredia@gmail.com>
2020-12-19 19:29:14 -0800

    Update Messengerscape.user.js
    Support Facebook Messenger redesign

--- docs/tampermonkey-scripts/Messengerscape.user.js
+++ docs/tampermonkey-scripts/Messengerscape.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Messengerscape
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Press Escape to exit a Facebook Messenger DM and return to Inbox
 // @author       You
 // @match        https://www.facebook.com/messages*
@@ -12,18 +12,34 @@
     'use strict';
 
     function clickConvoMenuItem(label) {
-        Array.from(document.querySelectorAll('._54nf ._54ni')).find(el => el.textContent === label).click();
+        Array.from(document.querySelectorAll('[role="menuitem"]')).find(el => el.textContent === label).click();
     }
 
     function exitAllConvos() {
         const newMsgBtn = document.querySelector('[aria-label="New Message"]');
         newMsgBtn.click();
+        setTimeout(() => {
+            const newMsgDropdown = document.querySelector('[role="listbox"]');
+            newMsgDropdown.style.display = "none";
+        }, 100);
     }
 
     function openConvoActionsMenu() {
         // Open conversation actions menu
-        // document.querySelector('._23_m ._5blh').click(); - this selector doesnt work when 1st clicking a previously hidden convo
-        document.querySelector('._1ht2 ._5blh').click();
+        const activeThreadName = getActiveThreadName();
+        console.log({activeThreadName});
+        if (!activeThreadName) {
+            return;
+        }
+
+        const activeThreadNode = Array.from(document.querySelectorAll('[data-testid="mwthreadlist-item"]'))
+            .find(el => el.textContent.includes(activeThreadName));
+        console.log({activeThreadNode: activeThreadNode.textContent});
+        activeThreadNode.querySelector('[aria-label="Menu"]').click();
+    }
+
+    function getActiveThreadName() {
+        return document.querySelector('[role="main"] span:not([data-visualcompletion="ignore"])').innerText;
     }
 
     document.addEventListener('keydown', evt => {
@@ -44,16 +60,23 @@
         // Command + H -> Hide currently selected conversation
         if (evt.metaKey && evt.key === 'h') {
             openConvoActionsMenu();
-            setTimeout(() => clickConvoMenuItem('Hide'), 100);
+            setTimeout(() => clickConvoMenuItem('Hide Conversation'), 100);
         }
 
         // Command + F -> Give focus to first conversation in list
         if (evt.metaKey && evt.key === 'f') {
             evt.preventDefault();
-            const convoList = document.querySelector('[aria-label="Conversation List"]');
-            if (convoList) {
-                convoList.firstChild.focus();
+            const firstThread = document.querySelector('[data-testid="mwthreadlist-item"] [role="link"]');
+            if (firstThread) {
+                firstThread.tabIndex = 0;
+                firstThread.focus();
             }
+        }
+
+        // Command + K -> Give focus to Messenger searchbox
+        if (evt.metaKey && evt.key === 'k') {
+            const searchBox = document.querySelector('[aria-label="Search Messenger"]');
+            searchBox.focus();
         }
     });
 })();
