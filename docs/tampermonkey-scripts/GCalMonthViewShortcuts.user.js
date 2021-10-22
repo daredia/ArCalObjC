7c6cae1b7c6846d791266643d52481f019961166
Shehzad Daredia <shehzad.daredia@gmail.com>
2021-10-21 18:57:27 -0700

    Update GCalMonthViewShortcuts.user.js

--- docs/tampermonkey-scripts/GCalMonthViewShortcuts.user.js
+++ docs/tampermonkey-scripts/GCalMonthViewShortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GCal Month View Shortcuts
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://calendar.google.com/calendar/*/*/r/month*
